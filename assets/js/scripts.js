c82bd7659027efd5db9c9cd773844379fc47446e
SamVJulius <samsonvjulius@gmail.com>
2023-10-03 22:26:16 +0530

    countdown timer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -303,7 +303,7 @@
 	});
 
 	/*-------------------- COUNTDOWN -------------------*/
-	if( $( '#clockdiv' )[0] ){
+	if ($('#clockdiv')[0]) {
 		function getTimeRemaining(endtime) {
 			var t = Date.parse(endtime) - Date.parse(new Date());
 			var seconds = Math.floor((t / 1000) % 60);
@@ -318,34 +318,36 @@
 				'seconds': seconds
 			};
 		}
-
+	
 		function initializeClock(id, endtime) {
 			var clock = document.getElementById(id);
 			var daysSpan = clock.querySelector('.days');
 			var hoursSpan = clock.querySelector('.hours');
 			var minutesSpan = clock.querySelector('.minutes');
 			var secondsSpan = clock.querySelector('.seconds');
-
+	
 			function updateClock() {
 				var t = getTimeRemaining(endtime);
-
+	
 				daysSpan.innerHTML = t.days;
 				hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
 				minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
 				secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
+	
 				if (t.total <= 0) {
 					clearInterval(timeinterval);
 				}
 			}
-
+	
 			updateClock();
 			var timeinterval = setInterval(updateClock, 1000);
 		}
-
-		var deadline = new Date(Date.parse(new Date()) + 13 * 23 * 50 * 60 * 1000);
+	
+		// Set the deadline to October 6th, 0000 hours (midnight)
+		var deadline = new Date('2023-10-06T00:00:00');
 		initializeClock('clockdiv', deadline);
 	}
+	
 
 	/* -------------------- ISOTOPE ------------------- */
 	if( $( '.grid-gallery' )[0] ){
